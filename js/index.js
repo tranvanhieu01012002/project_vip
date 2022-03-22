84a2ded6b39eee4ab6fc0d662fd0b65d21dd1ddc
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-03-22 21:09:32 +0700

    upload for customer

--- js/index.js
+++ js/index.js
@@ -32,3 +32,37 @@ $(".container-slider").slick({
         // instead of a settings object
     ],
 });
+$(".container-slider-home").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ],
+});
